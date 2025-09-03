d72e3924f1b2f88d16be5084f507d63af30ea936
amuif <amudihayat@gmail.com>
2025-09-03 15:11:33 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  Logger.log(`Server running on port: ${port}`);
+}
+bootstrap();
